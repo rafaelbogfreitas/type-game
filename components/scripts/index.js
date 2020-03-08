a36c79fb5d6a6893cfe200395275e8fd88224e7a
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-03-08 12:22:19 -0300

    finishes organizing the game screen layout of elements

--- components/scripts/index.js
+++ components/scripts/index.js
@@ -1,9 +1,13 @@
 //Elements
 let startBtn = document.querySelector(".start-btn");//start button
 let startScreen = document.querySelector(".start-screen");//start screen
+let gameInput = document.querySelector(".game-input");
 
 //function that makes the start screen slide up off the viewport
-const liftStartScreen = () => startScreen.style.transform = "translateY(-100vh)";
+const liftStartScreen = () => {
+    startScreen.style.transform = "translateY(-110vh)";
+    gameInput.focus();
+}
 
 //Start button "click" event
 startBtn.addEventListener("click", liftStartScreen, false);
